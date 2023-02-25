af5520470e2d6ea48cf7847b25d83f36f6bb66b5
OLIMINATOR <oliminator@protonmail.com>
2023-02-25 21:03:14 +0100

    build(website): install dependencies

---
+++ packages/website/astro.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+    site: "https://svg.xhyrom.me/",
+    base: "/",
+    trailingSlash: "always",
+    output: "static",
+    integrations: [
+        sitemap(),
+        robotsTxt({
+            policy: [
+                {
+                    userAgent: "*",
+                },
+            ],
+            sitemap: true,
+        }),
+        compress({
+            css: true,
+            html: true,
+            img: false,
+            js: true,
+            svg: false,
+        }),
+        ,
+        tailwind(),
+    ],
+});
