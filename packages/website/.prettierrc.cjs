cd780f15a9bc528463f52f50f52237ebd58ab43a
OLIMINATOR <oliminator@protonmail.com>
2023-02-25 21:34:17 +0100

    fix(website): prettier

---
+++ packages/website/.prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro", "prettier-plugin-tailwindcss"),
+  ],
+
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
