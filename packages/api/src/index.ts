cf0547362f8aa57b8a60598025f454d6d342a51f
xHyroM <generalkubo@gmail.com>
2023-02-26 11:26:29 +0100

    docs(api): add source, license, read more into / route

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -17,7 +17,16 @@ await fastify.register(import("@fastify/compress"), {
 });
 
 fastify.get("/", async () => {
-    return fastify.printRoutes();
+    return (
+        fastify.printRoutes() +
+        "\n\n" +
+        String.raw`
+    Source: https://data.statistics.sk/api/
+    All data from ^ is covered by Creative Commons Attribution License (cc-by) 4.0 (https://creativecommons.org/licenses/by/4.0/legalcode)
+
+    Read more at https://slovak.statistics.sk/wps/portal/ext/aboutus/webpage/terms/!ut/p/z1/rVFLU8IwEP4tHnrM7NIUSo8pKo-po6A8mouTllRibVJoLMKvNzBewXHGve3st998D-CwAq5Fq96EVUaLD7envPe6SJ76cdxh-DibBDi-HzCMX6Z0nPRgARx4rm1tN5CarBEb0pQeNtYx5CQ32kptPXRkjYd7mZlWkMbuhC6Fh7VZV0rq8kBq83lUrdBKnPjqXK0h9YvI7wraI6EfFCRAmZEslAGRvoxo4RcYdQQsfxPI3RkvDEP3z8-QwZCNgjBB7CfDLo7ZaD6LppQioz-AKxyp0xBe1HDbhWWr5B7m2uwql-nzHy2OECZnCVdcup7U-3bLmSvjlPmXhdW_tFFXVZ8eSFk83NGApzffxqSEBQ!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/
+    `
+    );
 });
 
 fastify.listen({ port: 3001 }, (err, address) => {
